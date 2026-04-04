8f1f5cb806cd1c2faf47c14f77ddbe44d2bbab43
postyizhan <185839426@qq.com>
2026-04-04 17:10:44 +0800

    fix: a better category example

--- src/main/java/com/customnpcs/craftingview/Config.java
+++ src/main/java/com/customnpcs/craftingview/Config.java
@@ -28,7 +28,7 @@ public class Config {
                 cfg.get(
                     "categories",
                     "category_0",
-                    "示例分类|1,2,3|").comment = "格式: 分类名|recipeId1,recipeId2,...|recipeName1,recipeName2,...  留空表示不过滤";
+                    "example||Gun Wooden,Gun Stone,Gun Iron,Gun Gold,Gun Diamond,Gun Emerald,Gun Bronze|").comment = "格式: 分类名|recipeId1,recipeId2,...|recipeName1,recipeName2,...  留空表示不过滤";
                 count = 1;
             }
 
